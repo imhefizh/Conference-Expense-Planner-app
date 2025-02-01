4652c991a5e8a8d705f764c98775d31947c315ac
Maulana Hafidz Ismail <hafidzsml@gmail.com>
2025-02-01 14:55:41 +0700

    fourth init

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Conference-Expense-Planner-app",
+  base: "/Learning-React",
   plugins: [react()],
 });
